37058acc24688138078623029220fe1c3964302e
Wu Xingbo <wuxb45@gmail.com>
2018-10-10 23:52:48 -0500

    fix bad_mmap

--- bad_mmap.c
+++ bad_mmap.c
@@ -2,19 +2,19 @@
 #include"user.h"
 int main(int argc, char** argv) {
   printf(1,"About to make first mmap. Next, you should see the first sentence from README\n");
-  int fd = open("README",0);  
+  int fd = open("README",0);
   char* text = mmap(fd,1);
   text[85]=0;
-  if(text!=(void*)0x40000000)
+  if(text!=(void*)0x400000000000lu)
     printf(1,"Returned pointer is %d, should be 1073741824 (0x40000000)\n",text);
   printf(1,"%s\n",text);
 
   printf(1,"\nSecond mmap coming\n");
-  int fd2 = open("LARGE",0);  
+  int fd2 = open("LARGE",0);
   char* text2 = mmap(fd2,1);
-  
+
   text2[71680]=0;
-  if(text2!=(void*)0x40001000)
+  if(text2!=(void*)0x400000001000lu)
     printf(1,"Returned pointer is %d, should be 1073741824+4069 (0x40001000)\n",text2);
   printf(1,"%s\n",text2+71661);
 
